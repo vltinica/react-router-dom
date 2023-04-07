eddb317e8b580490ec36949fe9dc0ec726dce892
Vladimir Tinica <vltinica@gmail.com>
2023-04-07 10:05:56 -0400

    float navBar 960px

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import {Routes, Route} from "react-router-dom";
+import Home from "./components/Home";
+
+
 
 function App() {
   return (
-    <div>
-      <h1>Vladimir</h1>
-    </div>
+    <>
+    <Routes>
+      <Route path="/"  element={<Home/>} />
+    </Routes>
+    </>
   );
 }
 
