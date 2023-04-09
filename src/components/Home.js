1613c64e2aee92334cf131cb395e36afa3add79f
Vladimir Tinica <vltinica@gmail.com>
2023-04-09 10:23:45 -0400

    add Hero commponent

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./HomeStyles.css"
 import Navbar from './Navbar'
+import Hero from './Hero'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
+      <Hero/>
     </div>
   )
 }
